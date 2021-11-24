10b366de5ae9d43b10b8b4ab74512682fd769a25
naeem <n3eem.1999@gmail.com>
2021-11-24 17:49:21 +0200

    first task in android

---
+++ app/src/main/java/com/nmg/bmi_app/homeActivity.java
@@ -0,0 +1,25 @@
+package com.nmg.bmi_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class homeActivity extends AppCompatActivity {
+Button btn_addNewRecord;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btn_addNewRecord = findViewById(R.id.btn_addNewRecord);
+
+        btn_addNewRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),newRecordActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/nmg/bmi_app/newRecordActivity.java
@@ -0,0 +1,14 @@
+package com.nmg.bmi_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newRecordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_record);
+    }
+}
